868b92b0704a2717cf4e29467ad7bc67f6902114
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-05-16 19:38:04 +0200

    Update: Readme.txt, build: basic structure for React

---
+++ st21-what-the-wine/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainPage from './mainpage'
+import './index.css';
+
+ReactDOM.render(
+  <MainPage/>
+  ,
+  document.getElementById('root')
+);
+
+
