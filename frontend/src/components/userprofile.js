3b080c2698bd16f02e35743357790f67e50d5377
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-04 14:51:30 +0200

    added user profile page

---
+++ frontend/src/components/userprofile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserProfile = () => {
+return (
+<div>
+<h1>Your Profile</h1>
+<p>Enter your choice here and get a recommendation!</p>
+</div>
+)  
+};
+
+export {UserProfile as default };
+
+// The Profile Page of the User
