633043b0ee98c72899b0bb10e7ef57fac4fc03b5
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-04 14:45:27 +0200

    added forms to sign up and login

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const Login = () => (
-    <div>
+const Login = () => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log("Form submitted");
+    }
 
-    </div>
-);
+    return (
+            <div>
+            <form method="post" action="/st21-what-the-wine/public/index.html" onSubmit={handleSubmit}>
+            <fieldset>
+            <legend>Log In</legend>
+                <p>
+                    <label name="email">E-Mail Adress</label>
+                    <input type="email" name="email"></input>
+                </p>
+                <p>
+                    <label name="password">Create Password</label>
+                    <input type="password" name="password"></input>
+                </p>
+                <p>
+                <button type="submit">Log In</button>
+                <input type="reset" value="Reset the filter"></input>
+                </p>
+                
+            </fieldset>
+            </form>
+            </div>
+
+    )
+};
     
     export {Login as default };
 
--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,10 +1,51 @@
 import React from 'react';
 
-const SignUp = () => (
-    <div>
+const SignUp = () => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log("Form submitted");
+    }
 
-    </div>
-);
+    return (
+            <div>
+            <form method="post" action="/st21-what-the-wine/public/index.html" onSubmit={handleSubmit}>
+            <fieldset>
+            <legend>Sign Up for personalized Wine Recommendations</legend>
+                <p>
+                    <label name="firstname">First Name</label>
+                    <input type="text" name="firstname"></input>
+                </p>
+                <p>
+                    <label name="lastname">Last Name</label>
+                    <input type="text" name="lastname"></input>
+                </p>
+                <p>
+                    <label name="email">E-Mail Adress</label>
+                    <input type="email" name="email"></input>
+                </p>
+                <p>
+                    <label name="password">Create Password</label>
+                    <input type="password" name="password"></input>
+                </p>
+                <p>
+                    <label name="passwordcheck">Check Password</label>
+                    <input type="password" name="passwordcheck"></input>
+                </p>
+                <p>
+                    <input type="checkbox" name="newsletter"></input>
+                    <label name="newsletter">Subscribe me to the Newsletter.</label>
+                </p>
+                <p>
+                <button type="submit">Sign me up</button>
+                <input type="reset" value="Reset the filter"></input>
+                </p>
+                
+            </fieldset>
+            </form>
+            </div>
+
+    )
+};
     
     export {SignUp as default };
 
