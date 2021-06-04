10a3f3914c9d44bce802b8345ca24897c58f950e
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-04 19:45:14 +0200

    declare fetch methods

---
+++ frontend/src/actions/wines.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const backendURL = process.env.REACT_APP_BACKENDURL;
+
+const fetchAllWines = async () => {
+
+try {
+
+const response = await axios.get(`${backendURL}/api/wines`);
+return response;
+
+} catch (error) {
+return error;
+}
+}
+
+const fetchSingleWines = async (id) => {
+
+    try {
+    
+    const response = await axios.get(`${backendURL}/api/wines/${id}`);
+    return response;
+    
+    } catch (error) {
+    return error;
+    }
+    }
+
+export {fetchAllWines, fetchSingleWines};
