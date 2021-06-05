37523050ed4422bcd8913e786aba33aa9297b643
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-05 07:47:15 +0200

    fix variable naming

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -14,13 +14,13 @@ const Search = () => {
         console.log("Form submitted");
 
         // obligated fields
-        foodpairing = evt.target.elements.foodpairing.value;
-        price = evt.target.elements.price.value;
-        winetype = evt.target.elements.winetype.value;
+        foodpairing = e.target.elements.foodpairing.value;
+        price = e.target.elements.price.value;
+        winetype = e.target.elements.winetype.value;
 
         // optional fields
-        sweetness = evt.target.elements.sweetness.value;
-        vegan = evt.target.elements.vegan.value;
+        sweetness = e.target.elements.sweetness.value;
+        vegan = e.target.elements.vegan.value;
 
         // testing if fields are empty, if not setting the state
         if (foodpairing && price && winetype) {
