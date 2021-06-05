0000e903fa7fabb53ad9b20e62094da3b21a9a40
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-05 07:45:35 +0200

    add state variables

--- frontend/src/components/userprofile.js
+++ frontend/src/components/userprofile.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {useParams} from 'react-router';
+import {fetchSingleUser} from '../actions/users';
 
 const UserProfile = () => {
+    const {id} = useParams();
+
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    useEffect(()=> {
+        fetchSingleUser(id).then((user) => {
+            console.log(user.data);
+            setEmail(user.data.email);
+            setFirstName(user.data.firstName);
+            setLastName(user.data.lastName);
+        })
+    }, []);
+
+
 return (
 <div>
-<h1>Your Profile</h1>
+<h1>`Your Profile: ${firstName} ${lastName}`</h1>
+<p>{email}</p>
 <p>Enter your choice here and get a recommendation!</p>
 </div>
 )  
