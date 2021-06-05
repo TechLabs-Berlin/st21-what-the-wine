376e08ff230feee1a00da94dda2a450f349f4978
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-05 07:39:00 +0200

    add semicolon

--- frontend/src/components/singleWine.js
+++ frontend/src/components/singleWine.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {useParams} from 'react-router'
+import {useParams} from 'react-router';
 import { fetchSingleWines } from '../actions/wines';
 
 const SingleWine = () => {
