02c81d3148dcb5911d14dc0c2aaa77227bed4ac4
ostend <smennear.nccu@gmail.com>
2021-06-07 13:55:21 +0200

    fix error in front end route, add to .gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 // Calling the Database
 
-connectDB();
+//connectDB();
 
 // const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extendednpm: true }));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', require('./routes/index'));
+app.use("/api", require("./routes/index"));
 
 app.listen(8080, () => {
-  console.log("It works woohooo!");
+  console.log("App is listening on port 8080...");
 });
--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -1,36 +1,33 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import MainPage from "../components/mainpage";
 import AllWines from "../components/allwines";
-import SingleWine from "../components/SingleWine";
+import SingleWine from "../components/singleWine";
 import AboutUs from "../components/about-us";
 import PageNotFound from "../components/pagenotfound";
 import Login from "../components/login";
 import SignUp from "../components/signup";
 
 const Router = () => {
-    return (
-<BrowserRouter>
-    <div>
-    <Header />
-    <Switch>
-    
-        <Route exact to path="/" component={MainPage} />
-        <Route path="/allwines" component={AllWines} /> 
-        <Route path="/wine/:id" component={SingleWine} /> 
-        <Route path="/about-us" component={AboutUs} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route component={PageNotFound} />
-    
-    </Switch>
-    <Footer />
-    </div>
-</BrowserRouter>
-    )};
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact to path="/" component={MainPage} />
+          <Route path="/allwines" component={AllWines} />
+          <Route path="/wine/:id" component={SingleWine} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-
-export {Router as default};
-
+export { Router as default };
