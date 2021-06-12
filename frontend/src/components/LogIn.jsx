dcfa668e1226cc3175764f02c0115921701d97ab
Timea Varju <miavarju011@gmail.com>
2021-06-12 20:53:06 +0200

    refactor log in page

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -7,29 +7,22 @@ const LogIn = () => {
   };
 
   return (
-    <div>
-      <form
-        method="post"
-        action="/st21-what-the-wine/public/index.html"
-        onSubmit={handleSubmit}
-      >
+    <>
+      <form method="post" action="" onSubmit={handleSubmit}>
         <fieldset>
           <legend>Log In</legend>
-          <p>
-            <label name="email">E-Mail Adress</label>
-            <input type="email" name="email"></input>
-          </p>
-          <p>
-            <label name="password">Create Password</label>
-            <input type="password" name="password"></input>
-          </p>
-          <p>
-            <button type="submit">Log In</button>
-            <input type="reset" value="Reset the filter"></input>
-          </p>
+          <label name="" htmlFor="email">
+            Email Address
+            <input type="email" id="email" name="email"></input>
+          </label>
+          <label name="password" htmlFor="password">
+            Password
+            <input type="password" id="password" name="password"></input>
+          </label>
+          <button type="submit">Log In</button>
         </fieldset>
       </form>
-    </div>
+    </>
   );
 };
 
