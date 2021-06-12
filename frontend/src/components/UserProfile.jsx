7ce0e93788dd52ae1920ecd1a20662a3195609cd
Timea Varju <miavarju011@gmail.com>
2021-06-12 20:51:49 +0200

    refactor static user profile page

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import Search from "./Search";
+import Header from "./Header";
+import { Link } from "react-router-dom";
 
 const UserProfile = () => {
   return (
-    <div>
-      <h1>Hello Anna!</h1>
-      <p>Let's find your perfect wine</p>
-      <Search />
-    </div>
+    <>
+      <Header />
+      <img src="" alt="profile picture"></img>
+      <h1>Anna</h1>
+      <section>
+        <div>
+          <div>15</div>
+          <div>wines saved</div>
+        </div>
+        <div>
+          <div>21</div>
+          <div>wines rated</div>
+        </div>
+      </section>
+      <section>
+        <h2>
+          <Link to="">Recommended for you</Link>
+        </h2>
+      </section>
+      <section>
+        <h2>Last filters</h2>
+      </section>
+      <section>
+        <h2>
+          <Link to="">Last viewed</Link>
+        </h2>
+      </section>
+    </>
   );
 };
 
