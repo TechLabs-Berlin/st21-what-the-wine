44db659c2d02adf46a6ce371316c9b78372b1713
Timea Varju <miavarju011@gmail.com>
2021-06-15 12:48:15 +0200

    delete pagenotfound component, fix routes

--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -1,35 +1,31 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import LandingLoggedIn from "../components/LandingLoggedIn";
 import UserProfile from "../components/UserProfile";
 import Wines from "../components/Wines";
 import WineDescription from "../components/WineDescription";
 import AboutUs from "../components/AboutUs";
-import PageNotFound from "../components/PageNotFound";
 import LogIn from "../components/LogIn";
 import SignUp from "../components/SignUp";
+import Footer from "../components/Footer";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <>
-        <Header />
-        <Switch>
-          <Route exact to path="/" component={LandingLoggedIn} />
-          <Route exact to path="/" component={UserProfile} />
-          <Route path="/Wines" component={Wines} />
-          <Route path="/WineDescription" component={WineDescription} />
-          <Route path="/AboutUs" component={AboutUs} />
-          <Route path="/LogIn" component={LogIn} />
-          <Route path="/SignUp" component={SignUp} />
-          <Route component={PageNotFound} />
-        </Switch>
-        <Footer />
-      </>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={LandingLoggedIn} />
+        <Route exact path="/" component={UserProfile} />
+        <Route path="/Wines" component={Wines} />
+        <Route path="/WineDescription" component={WineDescription} />
+        <Route path="/AboutUs" component={AboutUs} />
+        <Route path="/LogIn" component={LogIn} />
+        <Route path="/SignUp" component={SignUp} />
+      </Switch>
+      <Footer />
     </BrowserRouter>
   );
 };
 
-export { Router as default };
+export default Router;
