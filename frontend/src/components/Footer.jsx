ff23dfb7136a00fe228026dcec469abc25a39af0
Timea Varju <miavarju011@gmail.com>
2021-06-15 12:27:40 +0200

    rewrite footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Footer = () => (
-  <div>
-    <p>Copyright 2021 - The greatest, rocking What the Wine Team.</p>
-    <NavLink exact to="/">
-      Home
-    </NavLink>
-    <NavLink to="/Wines">See all Wines</NavLink>
-    <NavLink to="/AboutUs">About Us</NavLink>
-  </div>
-);
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="/Wines">Wines</NavLink>
+        <NavLink to="/AboutUs">About Us</NavLink>
+        <div>Copyright 2021 - The greatest, rocking What the Wine Team.</div>
+      </footer>
+    </>
+  );
+};
 
-export { Footer as default };
+export default Footer;
 
-// This is the footer with some information about the project.
+// This is the footer with some information about the project
