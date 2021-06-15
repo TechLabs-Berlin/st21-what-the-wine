2b9783a1448aa1fe09c50b7fc6da05c8d1d9a5ff
Timea Varju <miavarju011@gmail.com>
2021-06-15 12:33:33 +0200

    rewrite markup

--- frontend/src/components/WineDescription.jsx
+++ frontend/src/components/WineDescription.jsx
@@ -1,32 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import { fetchWineDescription } from "../actions/wines";
+import React from "react";
 
 const WineDescription = () => {
-  const { id } = useParams();
-
-  const [description, setDescription] = useState("");
-  useEffect(() => {
-    fetchWineDescription(id).then((wine) => {
-      console.log(wine.data);
-      setDescription(wine.data.description);
-    });
-  }, []);
-
   return (
-    <div>
+    <>
       <h1>This is wine XYZ</h1>
-      <p>This is a description about wine XYZ.</p>
-      <p>This is a description of the flavor profile.</p>
-      <p>Price</p>
-      <p>Type of Wine</p>
-      <p>Best Foodpairings</p>
-      <p>Community Rating</p>
-      <p>{description}</p>
-    </div>
+    </>
   );
 };
 
-export { WineDescription as default };
+export default WineDescription;
 
-// This component is to show a page for a single wine to get more details on it.
+// This component shows more details on a wine
--- frontend/src/components/Wines.jsx
+++ frontend/src/components/Wines.jsx
@@ -1,47 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { fetchWines } from "../actions/wines";
+import React from "react";
 
 const Wines = () => {
-  const [wines, setWines] = useState([]);
-
-  useEffect(() => {
-    fetchWines().then((wineList) => {
-      console.log(wineList?.data);
-      setWines(wineList?.data);
-    });
-  }, []);
-
-  return (
-    <div>
-      <h1>See all Wines</h1>
-      <p>This is a page with all the wines you can get.</p>
-      <ul>
-        {wines.map((wine) => {
-          return (
-            <Wine
-              id={wine._id}
-              name={wine.name}
-              price={wine.price}
-              description={wine.description}
-            />
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
-
-const Wine = ({ name, price, description, id }) => {
   return (
     <>
-      <Link to={`/wine/${id}`}>
-        <li>{`${name} for ${price}`}</li>
-      </Link>
+      <h1>See all Wines</h1>
     </>
   );
 };
 
-export { Wines as default };
+export default Wines;
 
-//This is an overview page of all the wines that exist.
+//This is an overview page of all the wines that exist in our database
