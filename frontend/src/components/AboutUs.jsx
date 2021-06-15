14bbb41753eed91d83a3b9359ce75996c5bab21d
Timea Varju <miavarju011@gmail.com>
2021-06-15 12:17:24 +0200

    rewrite about us page

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -2,34 +2,36 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <h1>About Us</h1>
-      <h2>Learn more about the team.</h2>
-      <div>
-        <h3>Data Science</h3>
-        <p>Marc Hendel & Juliana Gomes</p>
-      </div>
-      <div>
-        <h3>UX Design</h3>
-        <p>Ebunoluwa Meroyi, Berenika Godlewska & Ngoc Anh Doan Thi</p>
-      </div>
-      <div>
-        <h3>Web Development</h3>
-        <p>Timea Varju & Franziska Pendzialek</p>
-      </div>
-      <div>
-        <h2>About TechLabs</h2>
-        <p>
-          We dream of a world full of digital shapers. Individuals who use
-          technological tools to approach the challenges of our time with a
-          digital and entrepreneurial mindset.
-        </p>
-        <a href="https://www.techlabs.org/about">More about TechLabs</a>
-      </div>
-    </div>
+      <h2>Learn more about the team</h2>
+
+      <h3>Data Science</h3>
+      <section>Marc Hendel</section>
+      <section>Juliana Gomes</section>
+
+      <h3>UX Design</h3>
+      <section>Ebunoluwa Meroyi</section>
+      <section>Berenika Godlewska</section>
+      <section>Ngoc Anh Doan Thi</section>
+
+      <h3>Web Development</h3>
+      <section>Stephanie Mennear</section>
+      <section>Timea Varju</section>
+
+      <h2>About TechLabs</h2>
+      <p>
+        We dream of a world full of digital shapers. Individuals who use
+        technological tools to approach the challenges of our time with a
+        digital and entrepreneurial mindset.
+      </p>
+      <a href="https://www.techlabs.org/about" target="_blank">
+        More about TechLabs
+      </a>
+    </>
   );
 };
 
-export { AboutUs as default };
+export default AboutUs;
 
 //This is an about us page.
