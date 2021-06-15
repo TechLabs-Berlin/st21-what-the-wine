bf2d8eabd671069908a290c60e569ab1e47e5dce
Timea Varju <miavarju011@gmail.com>
2021-06-15 12:28:20 +0200

    fix export default

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,18 +3,19 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <h1>What the Wine</h1>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <NavLink to="/Wines">See all Wines</NavLink>
-      <NavLink to="/LogIn">Log In</NavLink>
-      <NavLink to="/SignUp">Sign Up</NavLink>
-    </header>
+    <>
+      <header>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="/Wines">Wines</NavLink>
+        <NavLink to="/LogIn">Log In</NavLink>
+        <NavLink to="/SignUp">Sign Up</NavLink>
+      </header>
+    </>
   );
 };
 
-export { Header as default };
+export default Header;
 
-// This is the Header including a navigation.
+// This is the header including navbar
