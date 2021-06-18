2af659491f1f4c1be8a4fd3397356f686d9263d1
Timea Varju <miavarju011@gmail.com>
2021-06-18 14:42:24 +0200

    redo recommendation form with Formik

--- frontend/src/components/RecommendationForm.jsx
+++ frontend/src/components/RecommendationForm.jsx
@@ -1,88 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Form, Field } from "formik";
 
 const RecommendationForm = () => {
-  // let [foodpairing, setFoodpairing] = useState("");
-  // let [price, setPrice] = useState("");
-  // let [vegan, setVegan] = useState("");
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log("Form submitted");
-
-    // obligated fields
-    // foodpairing = evt.target.elements.foodpairing.value;
-    // price = evt.target.elements.price.value;
-
-    // optional fields
-    // vegan = evt.target.elements.vegan.value;
-
-    // testing if fields are empty, if not setting the state
-    // if (foodpairing && price) {
-    //   setFoodpairing(foodpairing);
-    //   setPrice(price);
-    //   if (vegan) {
-    //     setVegan(vegan);
-    //     console.log(vegan);
-    //   }
-    // }
-  };
-
   return (
     <>
-      <form method="" action="" onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>I am having wine</legend>
-          <label>
-            with food
-            <input type="radio" name="foodpairing" value="with-food"></input>
-          </label>
+      <Formik
+        initialValues={{
+          foodpairing: "",
+          price: "",
+          vegan: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 100));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <fieldset>
+            <legend>I am having wine</legend>
+            <label>
+              with food
+              <Field type="radio" name="foodpairing" value="with-food" />
+            </label>
+            <label>
+              without food
+              <Field type="radio" name="foodpairing" value="without-food" />
+            </label>
+          </fieldset>
 
-          <label>
-            without food
-            <input type="radio" name="foodpairing" value="without-food"></input>
-          </label>
-        </fieldset>
+          <fieldset>
+            <legend>Price range</legend>
+            <label>
+              €<Field type="checkbox" name="price" value="low" />
+            </label>
+            <label>
+              €€
+              <Field type="checkbox" name="price" value="medium" />
+            </label>
+            <label>
+              €€€
+              <Field type="checkbox" name="price" value="high" />
+            </label>
+            <label>
+              €€€€
+              <Field type="checkbox" name="price" value="expensive" />
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <legend>Price range</legend>
-          <label>
-            €<input type="checkbox" name="price" value="low"></input>
-          </label>
+          <fieldset>
+            <legend>Looking for vegan options?</legend>
+            <label>
+              no
+              <Field type="radio" name="vegan" value="vegan-no" />
+            </label>
+            <label>
+              yes
+              <Field type="radio" name="vegan" value="vegan-yes" />
+            </label>
+          </fieldset>
 
-          <label>
-            €€
-            <input type="checkbox" name="price" value="medium"></input>
-          </label>
-
-          <label>
-            €€€
-            <input type="checkbox" name="price" value="high"></input>
-          </label>
-
-          <label>
-            €€€€
-            <input type="checkbox" name="price" value="expensive"></input>
-          </label>
-        </fieldset>
-
-        <fieldset>
-          <legend>Looking for vegan options?</legend>
-          <label>
-            no
-            <input type="radio" name="vegan" value="vegan-no"></input>
-          </label>
-
-          <label>
-            yes
-            <input type="radio" name="vegan" value="vegan-yes"></input>
-          </label>
-        </fieldset>
-
-        <div>
-          <button type="button">More filters</button>
-          <button type="submit">Go</button>
-        </div>
-      </form>
+          <div>
+            <button type="button">More filters</button>
+            <button type="submit">Go</button>
+          </div>
+        </Form>
+      </Formik>
     </>
   );
 };
