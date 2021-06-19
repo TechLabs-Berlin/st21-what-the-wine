93f6a07089093384225b682f394f6c4d12f7e721
Timea Varju <miavarju011@gmail.com>
2021-06-19 19:55:07 +0200

    remove unnecessary react fragments from the forms

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -3,33 +3,31 @@ import { Formik, Form, Field } from "formik";
 
 const LogIn = () => {
   return (
-    <>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 100));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <fieldset>
-            <legend>Log In</legend>
-            <label>
-              Email Address
-              <Field type="email" name="email" />
-            </label>
-            <label>
-              Password
-              <Field type="password" name="password" />
-            </label>
-            <button type="submit">Log In</button>
-          </fieldset>
-        </Form>
-      </Formik>
-    </>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 100));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <fieldset>
+          <legend>Log In</legend>
+          <label>
+            Email Address
+            <Field type="email" name="email" />
+          </label>
+          <label>
+            Password
+            <Field type="password" name="password" />
+          </label>
+          <button type="submit">Log In</button>
+        </fieldset>
+      </Form>
+    </Formik>
   );
 };
 
--- frontend/src/components/RecommendationForm.jsx
+++ frontend/src/components/RecommendationForm.jsx
@@ -3,69 +3,67 @@ import { Formik, Form, Field } from "formik";
 
 const RecommendationForm = () => {
   return (
-    <>
-      <Formik
-        initialValues={{
-          foodpairing: "",
-          price: "",
-          vegan: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 100));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <fieldset>
-            <legend>I am having wine</legend>
-            <label>
-              with food
-              <Field type="radio" name="foodpairing" value="with-food" />
-            </label>
-            <label>
-              without food
-              <Field type="radio" name="foodpairing" value="without-food" />
-            </label>
-          </fieldset>
+    <Formik
+      initialValues={{
+        foodpairing: "",
+        price: "",
+        vegan: "",
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 100));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <fieldset>
+          <legend>I am having wine</legend>
+          <label>
+            with food
+            <Field type="radio" name="foodpairing" value="with-food" />
+          </label>
+          <label>
+            without food
+            <Field type="radio" name="foodpairing" value="without-food" />
+          </label>
+        </fieldset>
 
-          <fieldset>
-            <legend>Price range</legend>
-            <label>
-              €<Field type="checkbox" name="price" value="low" />
-            </label>
-            <label>
-              €€
-              <Field type="checkbox" name="price" value="medium" />
-            </label>
-            <label>
-              €€€
-              <Field type="checkbox" name="price" value="high" />
-            </label>
-            <label>
-              €€€€
-              <Field type="checkbox" name="price" value="expensive" />
-            </label>
-          </fieldset>
+        <fieldset>
+          <legend>Price range</legend>
+          <label>
+            €<Field type="checkbox" name="price" value="low" />
+          </label>
+          <label>
+            €€
+            <Field type="checkbox" name="price" value="medium" />
+          </label>
+          <label>
+            €€€
+            <Field type="checkbox" name="price" value="high" />
+          </label>
+          <label>
+            €€€€
+            <Field type="checkbox" name="price" value="expensive" />
+          </label>
+        </fieldset>
 
-          <fieldset>
-            <legend>Looking for vegan options?</legend>
-            <label>
-              no
-              <Field type="radio" name="vegan" value="vegan-no" />
-            </label>
-            <label>
-              yes
-              <Field type="radio" name="vegan" value="vegan-yes" />
-            </label>
-          </fieldset>
+        <fieldset>
+          <legend>Looking for vegan options?</legend>
+          <label>
+            no
+            <Field type="radio" name="vegan" value="vegan-no" />
+          </label>
+          <label>
+            yes
+            <Field type="radio" name="vegan" value="vegan-yes" />
+          </label>
+        </fieldset>
 
-          <div>
-            <button type="button">More filters</button>
-            <button type="submit">Go</button>
-          </div>
-        </Form>
-      </Formik>
-    </>
+        <div>
+          <button type="button">More filters</button>
+          <button type="submit">Go</button>
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
