5d6cb4f0805dbdd223849deb4dfc35e9b9e0ee28
Timea Varju <miavarju011@gmail.com>
2021-06-21 16:30:05 +0200

    add markup to new landing page

---
+++ frontend/src/components/Landing.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import RecommendationForm from "./RecommendationForm";
+
+const Landing = () => {
+  return (
+    <>
+      <figure>
+        <p>
+          Perfect
+          <br />
+          wine
+        </p>
+        <p>
+          Every.
+          <br />
+          Single.
+          <br />
+          Time.
+        </p>
+      </figure>
+      <main>
+        <p>Cheers!</p>
+        <h1>Let's find your perfect wine</h1>
+        <RecommendationForm />
+      </main>
+    </>
+  );
+};
+
+export default Landing;
+
+// The landing page
