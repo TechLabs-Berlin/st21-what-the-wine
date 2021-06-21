220752302c1a5c3babecb1b3435f441de314190f
Timea Varju <miavarju011@gmail.com>
2021-06-21 17:09:21 +0200

    update markup to match MVP wireframe

--- frontend/src/components/WineDescription.jsx
+++ frontend/src/components/WineDescription.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const WineDescription = () => {
-  return <h1>This is wine XYZ</h1>;
+  return (
+    <main>
+      <h1>Sauvignon Blanc</h1>
+      <p>Les Jamelles</p>
+
+      <figure>
+        <img src="" alt=""></img>
+        <div>€€</div>
+      </figure>
+
+      <section>
+        <h2>Flavour profile</h2>
+        <ul>
+          <li>
+            Sweetness <span>1</span>
+          </li>
+          <li>
+            Bitterness <span>3</span>
+          </li>
+          <li>
+            Acidity <span>2</span>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Type</h2>
+        <p>White</p>
+      </section>
+
+      <section>
+        <h2>Origin</h2>
+        <p>France</p>
+      </section>
+
+      <section>
+        <h2>Food pairings</h2>
+        <ul>
+          <li>Pasta</li>
+          <li>Poultry</li>
+          <li>Vegetarian</li>
+          <li>Fish</li>
+        </ul>
+      </section>
+    </main>
+  );
 };
 
 export default WineDescription;
