fd83d652e8f5a667a8e1db73374fe8a84aa74da0
Timea Varju <miavarju011@gmail.com>
2021-06-25 11:49:01 +0200

    parse query string to pass on to the backend

--- frontend/src/components/RecommendationList.jsx
+++ frontend/src/components/RecommendationList.jsx
@@ -2,25 +2,34 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 
-// comment out the 2 console logs to get a glimpse of what's in there :)
-
 const RecommendationList = () => {
   const [myData, setMyData] = useState(null);
   const navigationLocation = useLocation();
-  // console.log(navigationLocation);
 
   useEffect(() => {
-    // forward the values of the form submission and send a get request with them
     // localhost will be changed, just here while in development
     const getData = async () => {
+      // take out the query string from the URL and create an object out of those values
+      const queryString = new URLSearchParams(navigationLocation.search);
+      const queryObject = {};
+      queryString.forEach(function (value, key) {
+        queryObject[key] = value;
+      });
+
+      // --ask backend if it's ok with the frontend passing every value as a string
+      // queryObject.price_eur = queryObject.price_eur.split(",");
+      // queryObject.wine_type = queryObject.wine_type.split(",");
+      // queryObject.origin = queryObject.origin.split(",");
+      // queryObject.flavor_profile = queryObject.flavor_profile.split(",");
+
       const response = await axios.get("http://localhost:8080/api/wines", {
-        params: navigationLocation.state,
+        params: queryObject,
       });
       setMyData(response.data);
       // console.log(response.data);
     };
     getData();
-  }, [navigationLocation.state]);
+  }, [navigationLocation.search]);
 
   return (
     <main>
