1f2c65f670d720265c3281365d39f4ce92be7b61
Timea Varju <miavarju011@gmail.com>
2021-06-26 19:39:49 +0200

    extract form values to separate file

---
+++ frontend/src/constants.js
@@ -0,0 +1,57 @@
+export const WINE_QUERY_PARAMS = {
+  foodPairing: "food_pairing",
+  foodName: "food_name",
+  price: "price_eur",
+  vegan: "vegan",
+  wineType: "wine_type",
+  countryName: "country_name",
+  flavorProfile: "flavor_profile",
+};
+
+export const FOOD_PAIRING = {
+  true: "true",
+  false: "false",
+};
+
+export const FOOD_NAMES = {
+  pasta: "pasta",
+  pork: "pork",
+  cheese: "cheese",
+  beef: "beef",
+  fish: "fish",
+};
+
+export const PRICE = {
+  low: "low",
+  medium: "med",
+  high: "high",
+  expensive: "exp",
+};
+
+export const VEGAN = {
+  true: "true",
+  false: "false",
+};
+
+export const WINE_TYPE = {
+  red: "red",
+  white: "white",
+  rose: "rose",
+  sparkling: "sparkling",
+  dessert: "dessert",
+};
+
+export const COUNTRY_NAME = {
+  France: "France",
+  Germany: "Germany",
+  Italy: "Italy",
+  Portugal: "Portugal",
+  Spain: "Spain",
+  Other: "Other",
+};
+
+export const FLAVOR_PROFILE = {
+  dry: "dry",
+  sweet: "sweet",
+  acidic: "acidic",
+};
