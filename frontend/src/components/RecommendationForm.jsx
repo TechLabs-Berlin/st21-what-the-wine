6012b36373a0099b27561a9ab2c3e78634e4b28b
Timea Varju <miavarju011@gmail.com>
2021-07-06 23:22:09 +0200

    style switch toggle

--- frontend/src/components/RecommendationForm.jsx
+++ frontend/src/components/RecommendationForm.jsx
@@ -58,177 +58,247 @@ const RecommendationForm = () => {
         });
       }}
     >
-      <Form className="form-container">
-        <fieldset className="form-fieldset">
-          <legend className="form-title">I am having wine:</legend>
-          <label>
-            with food
-            <Field
-              type="radio"
-              name={WINE_QUERY_PARAMS.foodPairing}
-              value={FOOD_PAIRING.true}
-              checked={withFood}
-              onClick={handleWithFood}
-            />
-          </label>
-          <label>
-            without food
-            <Field
-              type="radio"
-              name={WINE_QUERY_PARAMS.foodPairing}
-              value={FOOD_PAIRING.false}
-              checked={withoutFood}
-              onClick={handleWithoutFood}
-            />
-          </label>
+      {({ values }) => (
+        <Form className="form-container">
+          <fieldset className="form-fieldset">
+            <legend className="form-title">I am having wine:</legend>
+            <label>
+              with food
+              <Field
+                type="radio"
+                name={WINE_QUERY_PARAMS.foodPairing}
+                value={FOOD_PAIRING.true}
+                checked={withFood}
+                onClick={handleWithFood}
+              />
+            </label>
+            <label>
+              without food
+              <Field
+                type="radio"
+                name={WINE_QUERY_PARAMS.foodPairing}
+                value={FOOD_PAIRING.false}
+                checked={withoutFood}
+                onClick={handleWithoutFood}
+              />
+            </label>
 
-          {withFood && (
-            <div className="form-with-food-container">
-              <SearchIcon />
-              <label>
-                Please specify...
-                <Field as="select" name={WINE_QUERY_PARAMS.foodName}>
-                  <option value="">Please specify...</option>
-                  <option value={FOOD_NAMES.pasta}>Pasta</option>
-                  <option value={FOOD_NAMES.pork}>Pork</option>
-                  <option value={FOOD_NAMES.cheese}>Cheese</option>
-                  <option value={FOOD_NAMES.beef}>Beef</option>
-                  <option value={FOOD_NAMES.fish}>Fish</option>
-                </Field>
+            {withFood && (
+              <div className="form-with-food-container">
+                <SearchIcon />
+                <label>
+                  Please specify...
+                  <Field as="select" name={WINE_QUERY_PARAMS.foodName}>
+                    <option value="">Please specify...</option>
+                    <option value={FOOD_NAMES.pasta}>Pasta</option>
+                    <option value={FOOD_NAMES.pork}>Pork</option>
+                    <option value={FOOD_NAMES.cheese}>Cheese</option>
+                    <option value={FOOD_NAMES.beef}>Beef</option>
+                    <option value={FOOD_NAMES.fish}>Fish</option>
+                  </Field>
+                </label>
+              </div>
+            )}
+          </fieldset>
+
+          <fieldset className="form-fieldset">
+            <legend className="form-title">Price range:</legend>
+            <div className="checkbox-label-container">
+              <label
+                className={
+                  values[WINE_QUERY_PARAMS.price]?.includes(PRICE.low)
+                    ? "checkbox-price-checked"
+                    : "checkbox-price"
+                }
+              >
+                €
+                <Field
+                  type="checkbox"
+                  name={WINE_QUERY_PARAMS.price}
+                  value={PRICE.low}
+                />
+              </label>
+              <label
+                className={
+                  values[WINE_QUERY_PARAMS.price]?.includes(PRICE.medium)
+                    ? "checkbox-price-checked"
+                    : "checkbox-price"
+                }
+              >
+                €€
+                <Field
+                  type="checkbox"
+                  name={WINE_QUERY_PARAMS.price}
+                  value={PRICE.medium}
+                />
+              </label>
+              <label
+                className={
+                  values[WINE_QUERY_PARAMS.price]?.includes(PRICE.high)
+                    ? "checkbox-price-checked"
+                    : "checkbox-price"
+                }
+              >
+                €€€
+                <Field
+                  type="checkbox"
+                  name={WINE_QUERY_PARAMS.price}
+                  value={PRICE.high}
+                />
+              </label>
+              <label
+                className={
+                  values[WINE_QUERY_PARAMS.price]?.includes(PRICE.expensive)
+                    ? "checkbox-price-checked"
+                    : "checkbox-price"
+                }
+              >
+                €€€€
+                <Field
+                  type="checkbox"
+                  name={WINE_QUERY_PARAMS.price}
+                  value={PRICE.expensive}
+                />
               </label>
             </div>
-          )}
-        </fieldset>
+          </fieldset>
 
-        <fieldset className="form-fieldset">
-          <legend className="form-title">Price range:</legend>
-          <label>
-            €
-            <Field
-              type="checkbox"
-              name={WINE_QUERY_PARAMS.price}
-              value={PRICE.low}
-            />
-          </label>
-          <label>
-            €€
-            <Field
-              type="checkbox"
-              name={WINE_QUERY_PARAMS.price}
-              value={PRICE.medium}
-            />
-          </label>
-          <label>
-            €€€
-            <Field
-              type="checkbox"
-              name={WINE_QUERY_PARAMS.price}
-              value={PRICE.high}
-            />
-          </label>
-          <label>
-            €€€€
-            <Field
-              type="checkbox"
-              name={WINE_QUERY_PARAMS.price}
-              value={PRICE.expensive}
-            />
-          </label>
-        </fieldset>
+          <fieldset className="form-fieldset" aria-labelledby="vegan">
+            <div className="fieldset-flex-container">
+              <legend className="form-title-flex" id="vegan">
+                Looking for vegan options?
+              </legend>
 
-        <fieldset className="form-fieldset">
-          <legend className="form-title">Looking for vegan options?</legend>
-          <label>
-            no
-            <Field
-              type="radio"
-              name={WINE_QUERY_PARAMS.vegan}
-              value={VEGAN.false}
-            />
-          </label>
-          <label>
-            yes
-            <Field
-              type="radio"
-              name={WINE_QUERY_PARAMS.vegan}
-              value={VEGAN.true}
-            />
-          </label>
-        </fieldset>
-
-        <div>
-          <button type="button" onClick={onToggleMoreFilters}>
-            {moreFilters ? "Show less filters" : "More filters"}
-          </button>
-
-          {moreFilters && (
-            <div className="form-more-filters-container">
-              <fieldset className="form-fieldset">
-                <legend className="form-title">Wine type</legend>
-                <DropdownIcon />
-                <label>
-                  Please select
-                  <Field as="select" name={WINE_QUERY_PARAMS.wineType}>
-                    <option value="">Please select</option>
-                    <option value={WINE_TYPE.red}>Red</option>
-                    <option value={WINE_TYPE.white}>White</option>
-                    <option value={WINE_TYPE.rose}>Rosé</option>
-                    <option value={WINE_TYPE.sparkling}>Sparkling</option>
-                    <option value={WINE_TYPE.dessert}>Dessert</option>
-                  </Field>
-                </label>
-              </fieldset>
-
-              <fieldset className="form-fieldset">
-                <legend className="form-title">Origin</legend>
-                <DropdownIcon />
-                <label>
-                  Please select
-                  <Field as="select" name={WINE_QUERY_PARAMS.countryName}>
-                    <option value="">Please select</option>
-                    <option value={COUNTRY_NAME.France}>France</option>
-                    <option value={COUNTRY_NAME.Germany}>Germany</option>
-                    <option value={COUNTRY_NAME.Italy}>Italy</option>
-                    <option value={COUNTRY_NAME.Portugal}>Portugal</option>
-                    <option value={COUNTRY_NAME.Spain}>Spain</option>
-                    <option value={COUNTRY_NAME.Other}>Other</option>
-                  </Field>
-                </label>
-              </fieldset>
-
-              <fieldset className="form-fieldset">
-                <legend className="form-title">Flavour profile</legend>
-                <label>
-                  rather dry
-                  <Field
-                    type="checkbox"
-                    name={WINE_QUERY_PARAMS.flavorProfile}
-                    value={FLAVOR_PROFILE.dry}
-                  />
-                </label>
-                <label>
-                  rather sweet
-                  <Field
-                    type="checkbox"
-                    name={WINE_QUERY_PARAMS.flavorProfile}
-                    value={FLAVOR_PROFILE.sweet}
-                  />
-                </label>
-                <label>
-                  rather acidic
-                  <Field
-                    type="checkbox"
-                    name={WINE_QUERY_PARAMS.flavorProfile}
-                    value={FLAVOR_PROFILE.acidic}
-                  />
-                </label>
-              </fieldset>
+              <Field
+                id="checkbox-switch"
+                type="checkbox"
+                name={WINE_QUERY_PARAMS.vegan}
+                className="switch-checkbox"
+              />
+              <label className="switch" htmlFor="checkbox-switch">
+                yes
+              </label>
             </div>
-          )}
+          </fieldset>
 
-          <button type="submit">Search</button>
-        </div>
-      </Form>
+          <div>
+            <button
+              className="button-more-filters"
+              type="button"
+              onClick={onToggleMoreFilters}
+            >
+              {moreFilters ? "Show less filters" : "More filters"}
+            </button>
+
+            {moreFilters && (
+              <div className="form-more-filters-container">
+                <fieldset className="form-fieldset" aria-labelledby="wine-type">
+                  <div className="fieldset-flex-container">
+                    <legend className="form-title-flex" id="wine-type">
+                      Wine type
+                    </legend>
+                    <DropdownIcon />
+                    <label className="sr-only">Please select</label>
+                    <Field
+                      as="select"
+                      name={WINE_QUERY_PARAMS.wineType}
+                      className="select"
+                    >
+                      <option value="">Please select</option>
+                      <option value={WINE_TYPE.red}>Red</option>
+                      <option value={WINE_TYPE.white}>White</option>
+                      <option value={WINE_TYPE.rose}>Rosé</option>
+                      <option value={WINE_TYPE.sparkling}>Sparkling</option>
+                      <option value={WINE_TYPE.dessert}>Dessert</option>
+                    </Field>
+                  </div>
+                </fieldset>
+
+                <fieldset className="form-fieldset" aria-labelledby="origin">
+                  <div className="fieldset-flex-container">
+                    <legend className="form-title-flex" id="origin">
+                      Origin
+                    </legend>
+                    <DropdownIcon />
+                    <label className="sr-only">Please select</label>
+                    <Field
+                      as="select"
+                      name={WINE_QUERY_PARAMS.countryName}
+                      className="select"
+                    >
+                      <option value="">Please select</option>
+                      <option value={COUNTRY_NAME.France}>France</option>
+                      <option value={COUNTRY_NAME.Germany}>Germany</option>
+                      <option value={COUNTRY_NAME.Italy}>Italy</option>
+                      <option value={COUNTRY_NAME.Portugal}>Portugal</option>
+                      <option value={COUNTRY_NAME.Spain}>Spain</option>
+                      <option value={COUNTRY_NAME.Other}>Other</option>
+                    </Field>
+                  </div>
+                </fieldset>
+
+                <fieldset className="form-fieldset">
+                  <legend className="form-title">Flavour profile</legend>
+                  <div className="checkbox-label-container">
+                    <label
+                      className={
+                        values[WINE_QUERY_PARAMS.flavorProfile]?.includes(
+                          FLAVOR_PROFILE.dry
+                        )
+                          ? "checkbox-flavor-checked"
+                          : "checkbox-flavor"
+                      }
+                    >
+                      dry
+                      <Field
+                        type="checkbox"
+                        name={WINE_QUERY_PARAMS.flavorProfile}
+                        value={FLAVOR_PROFILE.dry}
+                      />
+                    </label>
+                    <label
+                      className={
+                        values[WINE_QUERY_PARAMS.flavorProfile]?.includes(
+                          FLAVOR_PROFILE.sweet
+                        )
+                          ? "checkbox-flavor-checked"
+                          : "checkbox-flavor"
+                      }
+                    >
+                      sweet
+                      <Field
+                        type="checkbox"
+                        name={WINE_QUERY_PARAMS.flavorProfile}
+                        value={FLAVOR_PROFILE.sweet}
+                      />
+                    </label>
+                    <label
+                      className={
+                        values[WINE_QUERY_PARAMS.flavorProfile]?.includes(
+                          FLAVOR_PROFILE.acidic
+                        )
+                          ? "checkbox-flavor-checked"
+                          : "checkbox-flavor"
+                      }
+                    >
+                      acidic
+                      <Field
+                        type="checkbox"
+                        name={WINE_QUERY_PARAMS.flavorProfile}
+                        value={FLAVOR_PROFILE.acidic}
+                      />
+                    </label>
+                  </div>
+                </fieldset>
+              </div>
+            )}
+
+            <button className="button-search" type="submit">
+              Search
+            </button>
+          </div>
+        </Form>
+      )}
     </Formik>
   );
 };
